d4c422982ad8873c815c461a7367b56606a5cd64
jordantsanz <jordan.t.sanz.22@dartmouth.edu>
2020-10-01 23:01:53 -0500

    Starter environment

---
+++ src/components/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../style.scss';
+
+const App = () => <div className="test">Ivy hacks babe</div>;
+
+ReactDOM.render(<App />, document.getElementById('main'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,17 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const autoprefixer = require('autoprefixer');
 
 const env = process.env.NODE_ENV || 'development';
+// const autoprefixer = require('autoprefixer');
 // set to 'production' or 'development' in your env
 
 const finalCSSLoader = (env === 'production') ? MiniCssExtractPlugin.loader : { loader: 'style-loader' };
 
 module.exports = {
   mode: env,
+  output: { publicPath: '/' },
   entry: ['./src'], // this is where our app lives
   devtool: 'source-map', // this enables debugging with source in chrome devtools
-  devServer: {
-    hot: true,
-  },
   module: {
     rules: [
       {
@@ -22,20 +20,6 @@ module.exports = {
         use: [
           { loader: 'babel-loader' },
           { loader: 'eslint-loader' },
-          {
-            loader: 'file-loader',
-            options: {
-              useRelativePath: true,
-              name: '[name].[ext]',
-            },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: () => [autoprefixer()],
-              sourceMap: true,
-            },
-          },
         ],
       },
       {
@@ -54,6 +38,24 @@ module.exports = {
               sourceMap: true,
             },
           },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              useRelativePath: true,
+              name: '[name].[ext]',
+            },
+          },
         ],
       },
     ],
@@ -64,5 +66,13 @@ module.exports = {
       template: './src/index.html',
       filename: './index.html',
     }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './200.html',
+    }),
   ],
+  devServer: {
+    hot: true,
+    historyApiFallback: true,
+  },
 };
