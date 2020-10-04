f1207d90e834f2adf489e1b8c25b7965a5c8597d
jordantsanz <jordan.t.sanz.22@dartmouth.edu>
2020-10-04 12:18:05 -0500

    video backgrounds are in

--- src/components/LoadingElection.js
+++ src/components/LoadingElection.js
@@ -3,13 +3,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router';
+import video from '../images/votermag_h264.mp4';
 
 class LoadingElection extends Component {
   render() {
     if (this.props.electionData.length == 0 || this.props.electionData == undefined) {
       return (
         <div className="page-wrapper" id="page-4">
-          <div className="main-page-holder" id="page-4-holder">
+
+          <div className="special-main-page-holder" id="page-4-holder">
+            <video className="videoWhite" width="320" height="240" loop autoPlay muted id="video">
+              <source src={video} className="videoWhite" type="video/mp4" />
+            </video>
             <h1 className="title" id="page-4-title"> Finding your elections... </h1>
             <button type="button" className="button-red">I love democracy!</button>
           </div>
